452d342b1dc9ec03e42624ff79109072ec88ee06
unknown <rexyzav1@gmail.com>
2021-07-22 21:00:18 -0400

    my commit

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-management-system3"
+    key            = "demo.tfstate"
+    region         = "us-west-2"
+    profile        = "default"
+    dynamodb_table = "tf-state-file"
+  }
+}
